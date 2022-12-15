5ec226adae057009786962364467c2850f61632a
ObnoxiousOliver <maxhd2005@gmail.com>
2022-12-15 21:22:12 +0100

    file fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  build: {
-    format: 'file',
-  },
-  root: '.'
-});
+export default defineConfig({});
